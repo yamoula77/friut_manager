97d70c262f140b873df086ad23271d2f96690a19
yamoula77 <yamoulad.kankan@gmail.com>
2026-06-23 12:28:50 +0000

    ajout de la fonctionalite de prix

---
+++ data/app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from fruit_managers import *
+
+st.title("Dashboard de Plantation")
+
+inventaire = ouvrir_inventaire()
+prix = ouvrir_prix()
+tresorerie = ouvrir_tresorerie()
+
+st.header("La Tresorerie")
+st.metric(Label="Montant disponible", value=f"{tresorerie:.2f} $")
+
+st.header("inventaire")
+st.table(inventaire)
